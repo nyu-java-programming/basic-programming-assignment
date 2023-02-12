e58b1ea420346dd07e0bf071a7c151d390f46a98
bloombar <amos@cs.nyu.edu>
2023-02-12 18:08:32 -0500

    fixed instructions

--- src/edu/nyu/cs/CompoundInflation.java
+++ src/edu/nyu/cs/CompoundInflation.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
  * This question is inspired by Question 2-13 in Liang's Intro to Java textbook and current economic events.
  * 
  * Complete this program such that it:
- *  - prompts the user to enter an amount the user adds to their savings account each month
+ *  - prompts the user to enter an initial amount the user adds to their savings account
  *  - (use the Scanner class's nextLine() function to get user input as a String)
- *  - displays the equivalent value of the account, in today's dollars, after the sixth month of savings mixed with heavy inflation
+ *  - displays the equivalent value of the account, in today's dollars, after six months of heavy inflation
  *  - (note that today's dollars are worth more than future dollars, since dollars lose their purchasing power each month by the monthly inflation rate)
  *
  * Assume the following:
@@ -19,9 +19,9 @@ import java.util.Scanner;
  *  - use System.out.println() to output all text, not System.out.print()
  * 
  * Sample output, including example user input. Your program should format the output exactly like this: 
- *  Please enter your monthly savings rate: 
+ *  Please enter your initial savings deposit: 
  *  120
- *  If you save $120 per month with 10% annual inflation, after 6 months, your account will hold an amount equivalent to $114 today.
+ *  If you save $120 with 10% annual inflation, after 6 months, your account will hold an amount equivalent to $114 today.
  */
 public class CompoundInflation {
     
